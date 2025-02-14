d53d94583b9a0aff4af45623747a2e5b48c63b61
AyseMirayErmis57 <aysemirayermis.57@gmail.com>
2025-02-14 11:21:33 +0300

    Add files via upload

---
+++ 250214/ornek1.py
@@ -0,0 +1,8 @@
+asal_sayilar=[2,3,5,7,11,13,17,19,23]
+print(asal_sayilar[1:4])
+asal_sayilar=[2,3,5,7,11,13,17,19,23]
+print(asal_sayilar[5:])
+asal_sayilar=[2,3,5,7,11,13,17,19,23]
+print(asal_sayilar[:5])
+asal_sayilar=[2,3,5,7,11,13,17,19,23]
+print(asal_sayilar[0:6:2])
