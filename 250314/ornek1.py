83ec3b8813f9c8b2332bc5e1c2972d39e4954092
AyseMirayErmis57 <aysemirayermis.57@gmail.com>
2025-03-14 12:10:40 +0300

    Add files via upload

---
+++ 250314/ornek1.py
@@ -0,0 +1,8 @@
+miktar=int(input("lütfen kg girin"))
+if miktar<=10:
+     ucret=miktar*10
+     print("ödeme:",ucret)
+
+else:
+      ucret=100+(miktar-10)*7.5
+      print("odeme",ucret)
---
+++ 250314/ornek2.py
@@ -0,0 +1,6 @@
+kad= input("kullanici adinizi giriniz :")
+ksifre=input("kullanici sifresini girin:")
+if kad:=="Türkiye" and ksifre =="1923":
+       print("giris basarili")
+else:
+    print("kullanici adi yada şifre yanliş")
